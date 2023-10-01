11e33067786facd2197e73cd22397f66e47b0b81
Johannes Naylor <jonaylor89@gmail.com>
2023-10-01 18:13:13 +0200

    sessionId

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,8 +1,15 @@
+import { useSearchParams } from "next/navigation";
 
 const Results = () => {
+    const params = useSearchParams();
+    console.log({ params });
+
+    const sessionId = params.get('session_id');
+
     return (
         <div>
             <h1>Results</h1>
+            <div>{sessionId}</div>
         </div>
     );
 }
