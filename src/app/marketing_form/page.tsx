afc8c1115b3eeb137b40ffb31bd5081de9b05eea
Johannes Naylor <jonaylor89@gmail.com>
2023-10-01 18:08:03 +0200

    push up

--- src/app/marketing_form/page.tsx
+++ src/app/marketing_form/page.tsx
@@ -4,11 +4,15 @@ import Link from 'next/link';
 const MarketingForm = () => {
     return (
         <>
+        <div className='h-screen flex flex-col justify-center items-center'>
             <div>whoa</div>
             <Link
-                href=""
+                href="https://buy.stripe.com/test_aEUcOh0h32sWdeUfYY"
+                className='rounded-xl color-white bg-blue-500 p-4'
             >
+                submit
             </Link>
+        </div>
         </>
     );
 }
