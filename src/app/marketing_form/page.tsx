f75a546151a0469f23c6afd768f7b88fb54dfe42
Johannes Naylor <jonaylor89@gmail.com>
2023-10-01 18:18:41 +0200

    payment link var

--- src/app/marketing_form/page.tsx
+++ src/app/marketing_form/page.tsx
@@ -1,13 +1,24 @@
 
 import Link from 'next/link';
 
+const paymentLink = process.env.NEXT_PUBLIC_STRIPE_PAYMENT_LINK
+
 const MarketingForm = () => {
+    if (!paymentLink) {
+        return (
+            <div className='h-screen flex flex-col justify-center items-center'>
+                <div>whoa</div>
+                <div>no payment link</div>
+            </div>
+        );
+    }
+
     return (
         <>
         <div className='h-screen flex flex-col justify-center items-center'>
             <div>whoa</div>
             <Link
-                href="https://buy.stripe.com/test_aEUcOh0h32sWdeUfYY"
+                href={paymentLink}
                 className='rounded-xl color-white bg-blue-500 p-4'
             >
                 submit
