f13ad1b4f65e9bc872a6866a753b09d1d2b8de53
Armaan Hossain <armaanhossain213@gmail.com>
2023-10-05 01:21:55 -0400

    ui changes to landing page with filler content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,43 @@
-
-import Partners from '@/components/partners';
 import Link from 'next/link';
+import Partners from '@/components/partners';
 
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold text-blue-500">
-        get your music heard
-      </h1>
-      <div className="h-4"></div>
-      <h2>
-        create your unique marketing plan
-      </h2>
-      <div className="h-4"></div>
-      <Link
-        href="/marketing_form"
-        className="bg-white text-black font-black text-lg rounded-full px-4 py-2 hover:scale-105 transform transition-all duration-200 ease-in-out"
-      >start now</Link>
-      <div className="h-4"></div>
-      <Partners />
+    <main className="flex flex-col justify-center items-stretch h-screen relative">
+      <div className="flex flex-col justify-center items-center flex-grow m-4">
+        <h1 className="text-6xl m-2 font-bold text-[#63b2fd] text-center">
+          (Tapped AI Marketing logo)
+        </h1>
+        <h2 className="text-center font-semibold m-2">
+          create your unique marketing plan and
+        </h2>
+        <h1 className="text-4xl font-bold m-1 mb-2 text-[#1a5082] text-center">
+          get your music heard
+        </h1>
+
+        <Partners />
+      </div>
+
+      <div className="absolute bottom-0 left-0 w-full h-[100px] bg-white bg-opacity-10 relative">
+        <div 
+          className="absolute w-[150px] h-[75px] rounded-t-full bg-gray-700"
+          style={{
+            top: "-75px",
+            left: "calc(50% - 75px)"
+          }}
+        ></div>
+        <Link href="/marketing_form">
+          <div 
+            className="absolute w-[100px] h-[100px] rounded-full bg-[#63b2fd] flex items-center justify-center text-white font-bold cursor-pointer"
+            style={{
+              top: "-50px",
+              left: "calc(50% - 50px)"
+            }}
+          >
+            Start Now
+          </div>
+        </Link>
+      </div>
     </main>
   );
 }
--- src/components/partners.tsx
+++ src/components/partners.tsx
@@ -2,7 +2,7 @@
 const Partners = () => {
   return (
     <>
-      <div>death row, blank kanvaz, playmakrs entertainment</div>
+      <div className="text-center font-semibold">with marketing data from industry leaders such as (insert death row, blank kanvaz, playmakrs entertainment logos)</div>
     </>
   );
 };
