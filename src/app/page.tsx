c6cb8f40ae313a26e74c194375e4979ca37794be
Johannes Naylor <jonaylor89@gmail.com>
2023-10-05 20:25:43 -0400

    linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,19 +19,19 @@ export default function Home() {
       </div>
 
       <div className="absolute bottom-0 left-0 w-full h-[100px] bg-white bg-opacity-10 relative">
-        <div 
+        <div
           className="absolute w-[150px] h-[75px] rounded-t-full bg-gray-700"
           style={{
-            top: "-75px",
-            left: "calc(50% - 75px)"
+            top: '-75px',
+            left: 'calc(50% - 75px)',
           }}
         ></div>
         <Link href="/marketing_form">
-          <div 
+          <div
             className="absolute w-[100px] h-[100px] rounded-full bg-[#63b2fd] flex items-center justify-center text-white font-bold cursor-pointer"
             style={{
-              top: "-50px",
-              left: "calc(50% - 50px)"
+              top: '-50px',
+              left: 'calc(50% - 50px)',
             }}
           >
             Start Now
