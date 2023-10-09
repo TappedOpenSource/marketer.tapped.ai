d2cb097b110fb1a2d267e53618be63e671746af4
Johannes Naylor <jonaylor89@gmail.com>
2023-10-09 15:06:00 -0500

    coupon codes

--- src/app/preview/page.tsx
+++ src/app/preview/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { redirect, useSearchParams } from 'next/navigation';
+import { redirect, useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { marketingPlanListener } from '@/utils/database';
+import { getAccessCode, marketingPlanListener, useAccessCode } from '@/utils/database';
 import { MarketingPlan } from '@/types/marketing_plan';
 import Loading from '@/components/loading';
 import Processing from '@/components/processing';
@@ -12,6 +12,7 @@ import Link from 'next/link';
 const paymentLink = process.env.NEXT_PUBLIC_STRIPE_PAYMENT_LINK;
 
 const Preview = () => {
+  const router = useRouter();
   const params = useSearchParams();
   const clientReferenceId = params.get('client_reference_id');
   if (!clientReferenceId) {
@@ -21,6 +22,7 @@ const Preview = () => {
   const paymentUrl = `${paymentLink}?client_reference_id=${clientReferenceId}`;
 
   const [marketingPlan, setMarketingPlan] = useState<MarketingPlan | null>(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     console.log({ clientReferenceId });
@@ -28,6 +30,39 @@ const Preview = () => {
       setMarketingPlan(marketingPlan);
     });
   }, []);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const formData = new FormData(e.currentTarget);
+    const theCode = formData.get('accessCode');
+    console.log({ code: theCode.toString() });
+
+    // check if access code exists
+    const accessCode = await getAccessCode(theCode.toString().toLowerCase());
+    if (accessCode === null) {
+      alert(`This access code ${theCode} does not exist.`);
+      setLoading(false);
+      return;
+    }
+
+    // check if access code has been used before
+    const alreadyUsed = accessCode.used;
+    if (alreadyUsed) {
+      alert(`This access code ${theCode} has already been used.`);
+      setLoading(false);
+      return;
+    }
+
+    await useAccessCode({
+      code: theCode.toString().toLowerCase(),
+      clientReferenceId,
+    });
+
+    // if not, redirect them to results with client_reference_id and access_code
+    router.push(`/results?client_reference_id=${clientReferenceId}&access_code=${theCode}`);
+    setLoading(false);
+  };
 
   if (marketingPlan === null) {
     return (
@@ -48,14 +83,18 @@ const Preview = () => {
   console.log(headers);
   console.log(subheaders);
 
+  if (loading) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
     <div className="flex flex-col lg:flex-row px-4 py-4 lg:px-24 lg:pt-24">
       <div className='flex flex-col items-center'>
         <h1 className='text-6xl font-extrabold py-6 px-6 text-center'>Preview</h1>
         <p className='md:w-1/2 lg:w-full text-center font-thin pb-12'>
-          this is copy for ilias to add stuff.
-          he'll add some shit about how this is a pivotal
-          moment in the artist's career and how this could change their life
+        Take one step closer to being independent and not relying on record labels.
         </p>
         <div>
           <Link
@@ -64,6 +103,37 @@ const Preview = () => {
           >
             BUY FULL PLAN
           </Link>
+        </div>
+        <div className='h-8'></div>
+        <div className="grid w-full grid-cols-3 items-center my-4">
+          <div className="h-px bg-gray-300"></div>
+          <span className="text-center text-white">or</span>
+          <div className="h-px bg-gray-300"></div>
+        </div>
+        <div className='h-8'></div>
+        <div className='w-72'>
+          <div className="relative h-10 w-full min-w-[200px]">
+            <form onSubmit={onSubmitHandler}>
+              <div className="relative flex h-10 w-full min-w-[200px] max-w-[24rem]">
+                <input
+                  name="accessCode"
+                  className="lowercase peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 pr-20 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                  placeholder=" "
+                  required
+                />
+                <button
+                  className="!absolute right-1 top-1 z-10 select-none rounded bg-blue-600 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none peer-placeholder-shown:pointer-events-none peer-placeholder-shown:bg-blue-gray-500 peer-placeholder-shown:opacity-50 peer-placeholder-shown:shadow-none"
+                  type="submit"
+                  data-ripple-light="true"
+                >
+                  Submit
+                </button>
+                <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                  Access Code
+                </label>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
       <div className='h-8 w-4 lg:h-12 lg:w-12'></div>
--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -4,7 +4,7 @@ import Loading from '@/components/loading';
 import Processing from '@/components/processing';
 import { MarketingPlan } from '@/types/marketing_plan';
 import { checkoutSessionToClientReferenceId } from '@/utils/api';
-import { marketingPlanListener } from '@/utils/database';
+import { getAccessCode, marketingPlanListener } from '@/utils/database';
 import { redirect, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import Markdown from 'react-markdown';
@@ -13,22 +13,45 @@ import Markdown from 'react-markdown';
 const Results = () => {
   const params = useSearchParams();
   const sessionId = params.get('session_id');
-  if (!sessionId) {
+  const clientReferenceId = params.get('client_reference_id');
+  const accessCode = params.get('access_code');
+
+  if (!sessionId && !(clientReferenceId && accessCode)) {
     redirect('/');
   }
 
   const [marketingPlan, setMarketingPlan] = useState<MarketingPlan | null>(null);
 
   useEffect(() => {
-    const fetchClientReferenceId = async () => {
-      const clientReferenceId = await checkoutSessionToClientReferenceId(sessionId);
-      console.log({ clientReferenceId });
-      marketingPlanListener(clientReferenceId, async (marketingPlan) => {
-        setMarketingPlan(marketingPlan);
-      });
-    };
-    fetchClientReferenceId();
+    if (!sessionId) {
+      const fetchClientReferenceId = async () => {
+        const clientReferenceId = await checkoutSessionToClientReferenceId(sessionId);
+        console.log({ clientReferenceId });
+        marketingPlanListener(clientReferenceId, async (marketingPlan) => {
+          setMarketingPlan(marketingPlan);
+        });
+      };
+      fetchClientReferenceId();
+    }
   }, []);
+
+  useEffect(() => {
+    const startListener = async () => {
+      if (clientReferenceId && accessCode) {
+        const theCode = await getAccessCode(accessCode);
+        if (theCode === null) {
+          alert(`This access code ${accessCode} does not exist.`);
+          return;
+        }
+
+        console.log({ clientReferenceId });
+        marketingPlanListener(clientReferenceId, async (marketingPlan) => {
+          setMarketingPlan(marketingPlan);
+        });
+      }
+    };
+    startListener();
+  });
 
   if (marketingPlan === null) {
     return (
