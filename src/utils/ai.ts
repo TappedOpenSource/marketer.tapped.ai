af3447936eaa8d04e55182ff3e5d3036bf66f5d6
Johannes Naylor <jonaylor89@gmail.com>
2023-10-18 01:12:30 -0400

    text

--- src/utils/ai.ts
+++ src/utils/ai.ts
@@ -24,7 +24,7 @@ export const getAiFollowUpSteps = (content: string): {
         Here's the link: https://epk.tapped.ai/drake-42267246`,
       },
     ],
-    'launch a contest': [
+    'launch a contest on': [
       {
         title: 'social media manager',
         actions: `I've created a few of the image and captions for you to use on social media. 
@@ -32,7 +32,7 @@ export const getAiFollowUpSteps = (content: string): {
         Here's the link: https://socialmedia.tapped.ai/drake-42267246`,
       },
     ],
-    'create a series': [
+    'create a series of': [
       {
         title: 'social media manager',
         actions: `we don't support video just yet but herex's a reference to popular tiktok dances currently 
@@ -40,7 +40,7 @@ export const getAiFollowUpSteps = (content: string): {
         https://www.steezy.co/posts/7-tiktok-dance-moves-you-can-learn-at-home`,
       },
     ],
-    'host a live': [
+    'host a live twitter': [
       {
         title: 'social media manager',
         actions: `To run a Twitter Q&A, you can: 
@@ -61,7 +61,7 @@ export const getAiFollowUpSteps = (content: string): {
         here's a good reference https://passion.digital/blog/how-to-run-a-successful-twitter-qa/`,
       },
     ],
-    'partner with popular': [
+    'partner with popular fashion': [
       {
         title: 'stylist',
         actions: `your public appearance is going to be crucial for this release. 
@@ -69,31 +69,83 @@ export const getAiFollowUpSteps = (content: string): {
         here are some potential styles for you to wear https://stylist.tapped.ai/drake-42267246`,
       },
     ],
-    'reach out to': [
+    'reach out to nostalgic': [
       {
         title: 'marketer',
-        actions: 'blah blah',
+        actions: `we have a CRM of potential brands that might be great for you to work with.
+        
+        here's a potential email pitch template to use 
+
+        "Subject: Exciting Partnership Opportunity - Amplify Your Brand with My Music
+
+        Dear [Brand Contact's Name],
+        
+        I hope this email finds you well. I'm [Your Name], a [Your Genre] musician with a passion for [briefly mention your style, your values, or what sets you apart]. I'm reaching out because I believe there's a fantastic opportunity for us to collaborate and create something truly exceptional.
+        
+        As I prepare to release my upcoming album, [Album Name], I've been captivated by your brand's unique style, commitment to quality, and your ability to connect with your audience on a profound level. I believe that our shared dedication to creativity, authenticity, and self-expression makes us a perfect fit for a collaboration.
+        
+        Here's how I envision our partnership:
+        
+        [Detail how your music aligns with the brand]: My music [mention specific elements in your music that resonate with the brand's values or aesthetic], creating a powerful synergy that can resonate with our target audiences.
+        
+        [Propose the partnership concept]: I propose the creation of a limited-edition clothing line inspired by the themes and aesthetics of my album. This collaboration would not only be an exciting venture for both of us but also an opportunity to engage our fans in a unique way.
+        
+        [Mention your audience and reach]: My fan base includes [number] of dedicated followers who actively engage with my music and content on platforms like [list your social media profiles]. This partnership could expose your brand to a wider, music-loving audience.
+        
+        [Benefits for the brand]: Highlight how this partnership can benefit the clothing brand - increased brand visibility, the chance to tell a unique story, or the potential for increased sales.
+        
+        I'd love to discuss this opportunity further, share more about my music, and hear your thoughts on how we can collaborate for mutual success. Whether it's through exclusive merchandise, social media campaigns, or other creative ideas, I'm open to exploring various possibilities.
+        
+        Please let me know if you're interested and when you're available for a brief call or meeting. I'm excited about the potential of this partnership and look forward to the opportunity to work together to create something truly remarkable."
+
+        navigate here to reach out to those brands https://crm.tapped.ai/drake-42267246
+        `,
       },
     ],
-    'create limited edition': [
+    'create limited edition merchandise': [
       {
         title: 'marketer',
-        actions: 'blah blah',
+        actions: `we don't create merch for you now but here are some references for getting started with merch
+        
+        https://splice.com/blog/how-to-make-sell-merch-as-an-artist/`,
       },
     ],
-    'pitch the single': [
+    'pitch the single to': [
       {
         title: 'marketer',
-        actions: 'blah blah',
+        actions: 'head over to our pitch studio to pitch to the right curators https://pitch.tapped.ai/drake-42267246',
       },
     ],
-    'arrange interviews with': [
+    'arrange interviews with popular': [
       {
         title: 'marketer',
-        actions: 'blah blah',
+        actions: `Some noteworthy podcasts in the space are
+
+        "Song Exploder": Hosted by Hrishikesh Hirway, this podcast features musicians breaking down their songs, explaining the creative process, and sharing insights into the music-making journey.
+
+        "Dissect": This podcast, hosted by Cole Cuchna, delves deep into the meaning and stories behind iconic albums and songs, analyzing the lyrics, music, and cultural context.
+        
+        "Switched on Pop": Nate Sloan and Charlie Harding discuss the art and science of pop music, dissecting popular songs and exploring the musical techniques used in their creation.
+        
+        "The Joe Rogan Experience": While this podcast covers a wide range of topics, Joe Rogan occasionally hosts musicians and bands for in-depth conversations about their music and careers.
+        
+        "Tiny Desk Concerts" by NPR Music: While not a traditional podcast, NPR's Tiny Desk Concert series features live performances from a wide variety of artists. They sometimes discuss their work in brief interviews.
+        
+        "The Songwriting Podcast": This podcast, hosted by Bruce Wawrzyniak, features songwriters discussing their creative process, inspiration, and techniques for crafting songs.
+        
+        "Broken Record": Hosted by Malcolm Gladwell, Rick Rubin, and Bruce Headlam, this podcast explores the stories behind iconic music and musicians.
+        
+        "Disgraceland": This podcast, hosted by Jake Brennan, delves into the dark and often scandalous stories of famous musicians and bands.
+        
+        "The Trap Set with Joe Wong": This podcast features in-depth interviews with drummers and musicians from various genres, exploring their artistic journeys and experiences.
+        
+        "Sound Opinions": Jim DeRogatis and Greg Kot review and discuss the latest music releases, interview artists, and delve into the music industry.        
+
+        check out our CRM to reach out to these podcasts https://crm.tapped.ai/drake-42267246
+        `,
       },
     ],
-    'collaborate with music': [
+    'collaborate with music streaming': [
       {
         title: 'marketer',
         actions: `here are the most popular playlists for your genre and vibe: Late 90s/Early 2000s ESSENTIAL JAMS: By Gabrielle Langhorn, this playlist includes songs like "Gin and Juice" by Snoop Dogg, "Candy Shop" by 50 Cent, and "End of the Road" by Boyz II Men. 
@@ -109,10 +161,40 @@ export const getAiFollowUpSteps = (content: string): {
         nagivate here to create share your playlist pitches to the correct curators`,
       },
     ],
+    'reach out to popular': [
+      {
+        title: 'marketer',
+        actions: `Some noteworthy channels in the space are
+        
+        Anthony Fantano (The Needle Drop): Anthony Fantano is a well-respected music critic known for his in-depth album reviews and opinionated reactions.
+
+Shawn Cee: Shawn Cee provides reviews, reactions, and discussions on a wide range of music genres, often with a humorous and engaging style.
+
+ARTV (Beyond ARTV): Jon Denton, also known as ARTV, offers album reviews, countdowns, and reactions to various music genres.
+
+Lost in Vegas: These two friends, George and Ryan, react to and analyze songs from different genres, offering their unique perspective as two individuals with different musical tastes.
+
+Trap Lore Ross: Trap Lore Ross delves into the backstories of various hip-hop and rap artists, providing context and analysis for their music and careers.
+
+Rick Beato: Rick Beato is a musician and producer who offers insightful analysis, reactions, and tutorials on music theory, songwriting, and production.
+
+BIGQUINT: BIGQUINT delivers animated and passionate reactions to hip-hop and rap music, often creating entertaining content through his emotional responses.
+
+Todd in the Shadows: Todd provides pop song reviews with a humorous and satirical approach, often focusing on mainstream music hits.
+
+Pitchfork: Pitchfork is a well-established music publication that also has a YouTube channel where they review and discuss various music releases.
+
+theneedledrop (FANTANO): In addition to his main channel, Anthony Fantano has a second channel where he shares quicker reactions to new songs and albums.
+
+        Check out our CRM to reach out to these channels https://crm.tapped.ai/drake-42267246
+`,
+      },
+    ],
   };
 
-  const contentList = content.split(' ');
+  const contentList = content.split(/(\s+)/).filter((e) => e.trim().length > 0);
   const firstFourWords = contentList.slice(0, 4).join(' ').toLowerCase().trim();
+  //   console.log({ firstFourWords });
   const followUpSteps = theMap[firstFourWords];
 
   return followUpSteps ?? null;
